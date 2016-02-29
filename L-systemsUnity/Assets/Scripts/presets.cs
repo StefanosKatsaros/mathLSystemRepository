ab370ccf9df8595c6d101c5968ac6fce03231d09
StefanosKatsaros <skats051@gold.ac.uk>
2016-02-29 20:35:53 +0000

    ScriptOrganization

---
+++ L-systemsUnity/Assets/Scripts/presets.cs
@@ -0,0 +1,173 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class presets : MonoBehaviour {
+
+	public InputField inputField1;
+	public InputField inputField2;
+	public InputField inputField3;
+	public InputField inputField4;
+	public InputField inputField5;
+
+	public InputField inputFieldN;
+
+	public InputField savingField;
+	public InputField terminatorField;
+
+	public InputField consField1;
+	public InputField constraintField1;
+	public InputField cons2Field;
+	public InputField constraint2Field;
+
+	string x;
+	string[] lines;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void preset1(){
+		inputField1.text = "F";
+		inputField2.text = "";
+		inputField3.text = "F";
+		inputField4.text = "F[+F]F[-F]F";
+		inputField5.text = "";
+		inputFieldN.text = "5";
+		savingField.text = "[";
+		terminatorField.text = "]";
+		consField1.text = "+";
+		constraintField1.text = "25.7";
+		cons2Field.text = "-";
+		constraint2Field.text = "25.7";
+	}
+
+	public void preset2(){
+		inputField1.text = "F";
+		inputField2.text = "";
+		inputField3.text = "F";
+		inputField4.text = "F[+F]F[-F][F]";
+		inputField5.text = "";
+		inputFieldN.text = "5";
+		savingField.text = "[";
+		terminatorField.text = "]";
+		consField1.text = "+";
+		constraintField1.text = "20";
+		cons2Field.text = "-";
+		constraint2Field.text = "20";
+	}
+
+	public void preset3(){
+		inputField1.text = "F";
+		inputField2.text = "";
+		inputField3.text = "F";
+		inputField4.text = "FF-[-F+F+F]+[+F-F-F]";
+		inputField5.text = "";
+		inputFieldN.text = "4";
+		savingField.text = "[";
+		terminatorField.text = "]";
+		consField1.text = "+";
+		constraintField1.text = "22.5";
+		cons2Field.text = "-";
+		constraint2Field.text = "22.5";
+	}
+
+	public void preset4(){
+		inputField1.text = "X";
+		inputField2.text = "F";
+		inputField3.text = "X";
+		inputField4.text = "F[+X]F[-X]+X";
+		inputField5.text = "FF";
+		inputFieldN.text = "7";
+		savingField.text = "[";
+		terminatorField.text = "]";
+		consField1.text = "+";
+		constraintField1.text = "20";
+		cons2Field.text = "-";
+		constraint2Field.text = "20";
+	}
+
+	public void preset5(){
+		inputField1.text = "X";
+		inputField2.text = "F";
+		inputField3.text = "X";
+		inputField4.text = "F[+X][-X]FX";
+		inputField5.text = "FF";
+		inputFieldN.text = "7";
+		savingField.text = "[";
+		terminatorField.text = "]";
+		consField1.text = "+";
+		constraintField1.text = "25.7";
+		cons2Field.text = "-";
+		constraint2Field.text = "25.7";
+	}
+
+	public void preset6(){
+		inputField1.text = "X";
+		inputField2.text = "F";
+		inputField3.text = "X";
+		inputField4.text = "F-[[X]+X]+F[+FX]-X";
+		inputField5.text = "FF";
+		inputFieldN.text = "5";
+		savingField.text = "[";
+		terminatorField.text = "]";
+		consField1.text = "+";
+		constraintField1.text = "22.5";
+		cons2Field.text = "-";
+		constraint2Field.text = "22.5";
+	}
+
+	public void preset7(){
+		inputField1.text = "F";
+		inputField2.text = "";
+		inputField3.text = "F-F-F-F";
+		inputField4.text = "FF-F-F-F-FF";
+		inputField5.text = "";
+		inputFieldN.text = "4";
+		savingField.text = "[";
+		terminatorField.text = "]";
+		consField1.text = "+";
+		constraintField1.text = "90";
+		cons2Field.text = "-";
+		constraint2Field.text = "90";
+	}
+
+	public void preset8(){
+		inputField1.text = "F";
+		inputField2.text = "X";
+		inputField3.text = "F";
+		inputField4.text = "X+F+X";
+		inputField5.text = "F-X-F";
+		inputFieldN.text = "8";
+		savingField.text = "[";
+		terminatorField.text = "]";
+		consField1.text = "+";
+		constraintField1.text = "60";
+		cons2Field.text = "-";
+		constraint2Field.text = "60";
+	}
+
+	public void readFromFile()
+	{
+		lines = System.IO.File.ReadAllLines("writeDataHere.txt");
+		inputField1.text = lines[0];
+		inputField2.text = lines[1];
+		inputField3.text = lines[2];
+		inputField4.text = lines[3];
+		inputField5.text = lines[4];
+		inputFieldN.text = lines[5];
+		savingField.text = lines[6];
+		terminatorField.text = lines[7];
+		consField1.text = lines[8];
+		constraintField1.text = lines[9];
+		cons2Field.text = lines[10];
+		constraint2Field.text = lines[11];
+	}
+
+}
---
+++ L-systemsUnity/Assets/Scripts/startScript3d.cs
@@ -0,0 +1,367 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+//using System.Linq;
+
+public class startScript3d : MonoBehaviour {
+	
+	// 5 first input fields && 2 buttons
+	
+	public InputField inputField1;
+	public InputField inputField2;
+	string variable1;
+	string variable2;
+	char myChar1;
+	char myChar2;
+	
+	public InputField inputField3;
+	string starter;
+	char[] myStarterChar; 
+	
+	public InputField inputField4;
+	public InputField inputField5;
+	string ruleString1;
+	string ruleString2;
+	
+	char[] ruleChar1;
+	char[] ruleChar2;
+	
+	public InputField inputFieldN;
+	string nString;
+	int nInt;
+	// list for organizing 
+	
+	List<char> listA = new List<char>();
+	List<char> listB = new List<char>();
+	
+	
+	public List<char> listC = new List<char>();
+	
+	
+	bool first = true;
+	
+	
+	// constraints
+	
+	public InputField savingField;
+	string save;
+	char saver;
+	
+	public InputField terminatorField;
+	string terminate;
+	char terminator;
+	
+	public InputField consField1;
+	string constr1;
+	char con1;
+	
+	public InputField constraintField1;
+	string constrain1;
+	float constraint1;
+	
+	public InputField cons2Field;
+	string constr2;
+	char con2;
+	
+	public InputField constraint2Field;
+	string constrain2;
+	float constraint2;
+	
+	// unity objects
+	public GameObject adder;
+	
+	public GameObject theOrigin;
+	
+	public GameObject pointer;
+	
+	public Transform objectSpawn;
+	
+	public float rotation = 25.7F;
+	// other variables used throughout the code
+	int theBool = 0;
+	
+	public float countdown = 2.0f;
+	
+	public GameObject cami;
+	
+	bool leafRule = false;
+	public GameObject leaf;
+	
+	int x = 1;
+	int e = 0;
+	
+	float q;
+	
+	void Start () {
+		
+		
+	}
+	
+	void Update () {
+		
+		
+		if (theBool == 1) {
+			
+			// once init() is complete the program will be running in real time
+			countdown -= Time.deltaTime;
+			
+			if(countdown <= 0.0f){
+				creation();
+				countdown = 0.0000001f;
+			}
+			
+		}
+	}
+	
+	public void init()
+	{	
+		// checks all inputs and if existant stores them in chars
+		if (inputField1.text != "") {
+			variable1 = inputField1.text;
+			myChar1 = variable1 [0];
+		}
+		
+		if (inputField2.text != "") {
+			variable2 = inputField2.text;
+			myChar2 = variable2 [0];
+		}
+		
+		if (inputField3.text != "") {
+			starter = inputField3.text;
+			myStarterChar = starter.ToCharArray();
+		}
+		
+		if (inputField4.text != "") {
+			ruleString1 = inputField4.text;
+			ruleChar1 = ruleString1.ToCharArray ();
+		}
+		
+		if (inputField5.text != "") {
+			ruleString2 = inputField5.text;
+			ruleChar2 = ruleString2.ToCharArray ();
+		}
+		
+		// checks and stores the constraints that will be later used
+		if (savingField.text != "") {
+			save = savingField.text;
+			saver = save [0];
+		}
+		
+		if (terminatorField.text != "") {
+			terminate = terminatorField.text;
+			terminator = terminate [0];
+		}
+		
+		if (consField1.text != "") {
+			constr1 = consField1.text;
+			con1 = constr1 [0];
+		}
+		
+		if (constraintField1.text != "") {
+			constrain1 = constraintField1.text;
+			constraint1 = float.Parse(constrain1);
+		}	
+		
+		if (cons2Field.text != "") {
+			constr2 = cons2Field.text;
+			con2 = constr2 [0];
+		}
+		
+		if (constraint2Field.text != "") {
+			constrain2 = constraint2Field.text;
+			constraint2 =  float.Parse(constrain2);
+		}
+		
+		if (inputFieldN.text != "") {
+			nString = inputFieldN.text;
+			nInt = int.Parse(nString);
+		}
+		// ads the axiom to the list, the starting point that will only be added once
+		if (first) {
+			for(int p = 0; p < myStarterChar.Length; p++){
+				listA.Add (myStarterChar[p]);
+			}
+			first = false;
+		}
+		// the main program which will loop as many times as we wish and will create the final complete list
+		for (int k = 0; k < nInt; k++) {
+			
+			if (listA.Count != 0) {
+				for (int i = 0; i < listA.Count; i++) {
+					
+					if (listA [i] == myChar1 && inputField1.text != "" ) {
+						
+						for (int j = 0; j < ruleChar1.Length; j++) {
+							
+							listB.Add (ruleChar1 [j]);
+							
+						}
+					} else if (listA [i] == myChar2 && inputField2.text != "") {
+						for (int j = 0; j < ruleChar2.Length; j++) {
+							
+							listB.Add (ruleChar2 [j]);
+						}
+					} else {
+						listB.Add (listA [i]);
+					}
+				}
+				listA.Clear ();
+			}
+			else if (listB.Count != 0) {
+				for (int i = 0; i < listB.Count; i++) {
+					
+					if (listB [i] == myChar1 && inputField1.text != "") {
+						
+						for (int j = 0; j < ruleChar1.Length; j++) {
+							
+							listA.Add (ruleChar1 [j]);
+							
+						}
+					} else if (listB [i] == myChar2 && inputField2.text != "") {
+						for (int j = 0; j < ruleChar2.Length; j++) {
+							
+							listA.Add (ruleChar2 [j]);
+						}
+					} else {
+						listA.Add (listB [i]);
+					}
+				}
+				listB.Clear ();
+				
+			}
+			
+		}
+		
+		
+		if (listA.Count != 0) {
+			listC = new List<char>(listA);
+		} else if (listB.Count !=0){
+			listC = new List<char>(listB);
+		}
+		
+		theBool = 1;	
+	}
+	
+	
+	// Reload scene to remove existing building blocks
+	public void renew(){
+		Application.LoadLevel("l-system3DScene");
+	}
+	
+	
+	
+	//
+	void creation (){
+		
+		
+		int holder = listC.Count;
+		
+		
+		if (holder > e) {
+			char o = listC [e];
+			e++;
+			
+			if (o == saver) {
+				
+				
+				Instantiate (pointer, objectSpawn.position, objectSpawn.rotation);
+				if (GameObject.Find ("pointer" + x + "(Clone)")) {
+					pointer = GameObject.Find ("pointer" + x + "(Clone)");
+					
+					
+				} else if (GameObject.Find ("pointer" + x)) {
+					pointer = GameObject.Find ("pointer" + x);
+				} else {
+					//x = 1;
+					theOrigin.gameObject.name = "pointer" + x;
+					pointer = theOrigin;
+				}
+				
+				x++;
+				pointer.gameObject.name = "pointer" + x;
+				objectSpawn = pointer.transform;
+				//			objectSpawn = Transform.position(pointer);
+			}
+			
+			
+			if (o == terminator) {
+				
+				
+				if (GameObject.Find ("pointer" + x + "(Clone)")) {
+					pointer = GameObject.Find ("pointer" + x + "(Clone)");
+				} else if (GameObject.Find ("pointer" + x)) {
+					pointer = GameObject.Find ("pointer" + x);
+				} else {
+					x = 1;
+					theOrigin.gameObject.name = "pointer" + x;
+					pointer = theOrigin;
+				}
+				if(leafRule){
+					Instantiate (leaf, objectSpawn.position, objectSpawn.rotation);
+				}
+				Destroy (pointer);
+				x--;
+				
+				if (GameObject.Find ("pointer" + x)) {
+					pointer = GameObject.Find ("pointer" + x);
+				} else {
+					x = 1;
+					theOrigin.gameObject.name = "pointer" + x;
+					pointer = theOrigin;
+				}
+				objectSpawn = pointer.transform;
+			}
+			
+			
+			if (o == myChar1 || o == myChar2) {					
+				
+				//check to see if more than 1 pointers exist and if so set the last pointer so we can move it
+				if (GameObject.Find ("pointer" + x)) {
+					pointer = GameObject.Find ("pointer" + x);
+				} else {
+					x = 1;
+					theOrigin.gameObject.name = "pointer" + x;
+					pointer = theOrigin;
+				}
+				
+				objectSpawn = pointer.transform;										// CHECK HERE! FOR SOME REASON PROBABLY OBJECTSPAWN HAS NO VALUE EVEN IF WE DO ABOVE.
+				Instantiate (adder, objectSpawn.position, objectSpawn.rotation);
+				pointer.transform.Translate (Vector3.up * 0.37f);
+			}
+			
+			if (o == con1) {
+				
+				objectSpawn = pointer.transform;
+				pointer.transform.Rotate (Vector3.forward * (constraint1));
+				pointer.transform.Translate (Vector3.up * 0.30f);
+			}
+			
+			if (o == con2) {
+				
+				
+				objectSpawn = pointer.transform;
+				pointer.transform.Rotate (Vector3.forward * (-constraint2));
+				pointer.transform.Translate (Vector3.up * 0.30f);
+			}
+			
+		} 
+	}
+	
+	
+	public void leavesOnOff(){
+		if (leafRule) {
+			leafRule = false;
+		} else {
+			leafRule = true;
+		}
+	}
+
+	public void twoD(){
+		Application.LoadLevel("l-systemScene");
+	}
+	
+	public void threeD(){
+		Application.LoadLevel("l-system3DScene");
+	}
+}
